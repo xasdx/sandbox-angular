fad40a22dfe3345f3d6dc1558a51b6f4d0a61eff
_asdx_ <xasdx@tuta.io>
2018-03-12 15:44:47 +0000

    Init an empty, ES6-based angular project

---
+++ public/src/app/app.component.js
@@ -0,0 +1,17 @@
+import { Component } from "angular2/core"
+
+class AppComponent {
+
+  static get annotations() {
+    return [
+      new Component({
+        selector: "my-app",
+        template: "<h1>hello</h1>"
+      })
+    ]
+  }
+
+  constructor () {}
+}
+
+export { AppComponent }
---
+++ public/src/app/main.js
@@ -0,0 +1,6 @@
+import { bootstrap } from "angular2/platform/browser"
+import { AppComponent } from "./app.component"
+
+let boot = document.addEventListener("DOMContentLoaded", () => { bootstrap(AppComponent) })
+
+module.exports = boot
