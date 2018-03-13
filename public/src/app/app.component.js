83467dcc971f588edebc2e2c0729cda923d64f12
asdx <xasdx@tuta.io>
2018-03-13 10:52:35 +0100

    Bootstrap application using an NgModule

--- public/src/app/app.component.js
+++ public/src/app/app.component.js
@@ -1,14 +1,12 @@
-import { Component } from "angular2/core"
+import { Component } from "@angular/core"
 
 @Component({
   selector: "my-app",
   template: "<h1>hello {{name}}</h1>"
 })
-export default class AppComponent {
+export class AppComponent {
 
   name = "angular"
 
   constructor () {}
 }
-
-export { AppComponent }
--- public/src/index.js
+++ public/src/index.js
@@ -1,6 +1,4 @@
-require("!!script!es6-shim/es6-shim.min.js")
-require("!!script!angular2/bundles/angular2-polyfills.min.js")
-require("!!script!rxjs/bundles/Rx.umd.min.js")
-require("!!script!angular2/bundles/angular2-all.umd.min.js")
+require("zone.js")
+require("reflect-metadata")
 
-require("./app/main")
+require("./main")
