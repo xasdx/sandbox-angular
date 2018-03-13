4d83d8ec1f63493efeaf4108cc00a7c1594949e8
asdx <xasdx@tuta.io>
2018-03-13 11:07:54 +0100

    Implement two-way binding with ngModel

--- public/src/app/app.component.js
+++ public/src/app/app.component.js
@@ -2,11 +2,19 @@ import { Component } from "@angular/core"
 
 @Component({
   selector: "my-app",
-  template: "<h1>hello {{name}}</h1>"
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}}</h2>
+    <input [(ngModel)]="hero.name" placeholder="name">
+    <p>{{time}}</p>
+  `
 })
 export class AppComponent {
 
-  name = "angular"
+  title = "hero editor"
+  hero = { name: "arthas" }
 
-  constructor () {}
+  constructor () {
+    this.time = new Date().toISOString()
+  }
 }
