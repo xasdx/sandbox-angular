80266d8cfaa784cade85f370d5d34d9ccbaab328
_asdx_ <xasdx@tuta.io>
2018-04-05 07:34:11 +0000

    Clean up everything that is not clock related

--- public/src/app/app.module.js
+++ public/src/app/app.module.js
@@ -3,47 +3,22 @@ import { BrowserModule } from "@angular/platform-browser"
 import { FormsModule } from "@angular/forms"
 import { HttpModule } from "@angular/http"
 
-import { StoreModule } from "@ngrx/store"
-import { EffectsModule } from "@ngrx/effects"
-import { StoreDevtoolsModule } from "@ngrx/store-devtools"
-import { heroReducer } from "./reducers/hero.reducer"
-import { HeroEffects } from "./effects/hero.effects"
-
-import { InMemoryWebApiModule } from "angular2-in-memory-web-api"
-import { InMemoryDataService } from "./in-memory-data.service"
-
-import { AppRoutingModule } from "./app-routing.module"
-
 import { AppComponent } from "./app/app.component"
-import { DashboardComponent } from "./dashboard/dashboard.component"
-import { HeroesComponent } from "./heroes/heroes.component"
-import { HeroDetailComponent } from "./hero-detail/hero-detail.component"
-import { HeroSearchComponent } from "./hero-search/hero-search.component"
 import { ClockComponent } from "./clock/clock.component"
-import { HeroService } from "./hero.service"
 import { ClockPipe } from "./clock.pipe"
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    InMemoryWebApiModule.forRoot(InMemoryDataService),
-    AppRoutingModule,
-    StoreModule.provideStore({ heroes: heroReducer }),
-    StoreDevtoolsModule.instrumentOnlyWithExtension(),
-    EffectsModule.run(HeroEffects)
+    HttpModule
   ],
   declarations: [
     AppComponent,
-    DashboardComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    HeroSearchComponent,
     ClockComponent,
     ClockPipe
   ],
-  providers: [ HeroService ],
+  providers: [],
   bootstrap: [ AppComponent ]
 })
 export class AppModule {}
--- public/src/app/reducers/hero.reducer.js
+++
@@ -1,8 +0,0 @@
-import * as heroActions from "../actions/hero.actions"
-
-export function heroReducer(state = [], action) {
-  switch (action.type) {
-    case heroActions.LOAD_HEROES_SUCCESS: { return action.payload }
-    default: { return state }
-  }
-}
